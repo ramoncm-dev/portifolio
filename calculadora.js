840c2fe4ef156080d8c7b16b335ad0a87a88d23b
Ramon Carvalho de Mendonça <ramoncm.dev@gmail.com>
2026-07-13 16:54:19 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+let currentInput = '';
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+
+        if(value === 'C'){
+            currentInput = '';
+        } 
+        else if(value === '='){
+            try {
+                currentInput = eval(currentInput) || '';
+            } catch {
+                currentInput = 'Erro';
+            }
+        } 
+        else {
+            currentInput += value;
+        }
+
+        display.value = currentInput;
+    });
+});
